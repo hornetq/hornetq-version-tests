914208207d6cde2539dc8ed4b8ae4256d38bd65a
Andy Taylor <ataylor@redhat.com>
2011-09-29 14:31:03 +0100

    first draft of test framework

---
+++ src/test/java/org/hornetq/integration/SimpleConnectionIT.java
@@ -0,0 +1,28 @@
+package org.hornetq.integration;
+
+import org.hornetq.api.core.TransportConfiguration;
+import org.hornetq.api.core.client.ClientSession;
+import org.hornetq.api.core.client.ClientSessionFactory;
+import org.hornetq.api.core.client.HornetQClient;
+import org.hornetq.api.core.client.ServerLocator;
+import org.hornetq.core.remoting.impl.netty.NettyConnectorFactory;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:andy.taylor@jboss.com">Andy Taylor</a>
+ *         Date: 9/29/11
+ *         Time: 10:56 AM
+ */
+public class SimpleConnectionIT
+{
+    @Test
+    public void testSingleConnection() throws Exception
+    {
+        System.out.println("SimpleConnectionIT.testSingleConnection");
+        TransportConfiguration transportConfiguration = new TransportConfiguration(NettyConnectorFactory.class.getName());
+        ServerLocator locator = HornetQClient.createServerLocatorWithoutHA(transportConfiguration);
+        ClientSessionFactory factory = locator.createSessionFactory();
+        ClientSession clientSession = factory.createSession();
+        clientSession.close();
+    }
+}
