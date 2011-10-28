31c55e87a46ad6a7f4fa075ebc87eb7efa533408
Andy Taylor <ataylor@redhat.com>
2011-10-28 11:32:31 +0100

    added failover test

--- README.txt
+++ README.txt
@@ -2,4 +2,12 @@ to run the test suite simply run 'mvn verify'.
 
 you can change the hornetq version by setting the properties in the pom.xml.
 
+to run the failover test you need to enable remote mbean connection by:
+
+export MAVEN_OPTS="-Dcom.sun.management.jmxremote.port=3000 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+
+and then run the failover test by:
+
+mvn -Pfailover verify
+
 remember to install the maven-hornetq-plugin first.
---
+++ src/test/java/org/hornetq/failover/SimpleFailoverIFT.java
@@ -0,0 +1,98 @@
+/*
+* JBoss, Home of Professional Open Source.
+* Copyright 2010, Red Hat, Inc., and individual contributors
+* as indicated by the @author tags. See the copyright.txt file in the
+* distribution for a full listing of individual contributors.
+*
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2.1 of
+* the License, or (at your option) any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this software; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
+package org.hornetq.failover;
+
+import org.hornetq.api.core.management.HornetQServerControl;
+import org.hornetq.api.core.management.ObjectNameBuilder;
+import org.hornetq.core.config.impl.ConfigurationImpl;
+import org.hornetq.jms.client.HornetQJMSConnectionFactory;
+import org.hornetq.jms.client.HornetQQueue;
+import org.junit.Test;
+
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.management.MBeanServer;
+import javax.management.MBeanServerConnection;
+import javax.management.MBeanServerInvocationHandler;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+import javax.naming.InitialContext;
+import java.io.File;
+import java.lang.management.ManagementFactory;
+import javax.jms.Connection;
+import java.util.HashMap;
+import java.util.Hashtable;
+
+/**
+ * @author <a href="mailto:andy.taylor@jboss.org">Andy Taylor</a>
+ *         10/28/11
+ */
+public class SimpleFailoverIFT
+{
+
+    private final String JMX_URL = "service:jmx:rmi:///jndi/rmi://localhost:3000/jmxrmi";
+
+    @Test
+    public void simpleFailover() throws Exception
+    {
+        Thread.sleep(5000);
+        Connection c;
+        Hashtable<String, String> env = new Hashtable<String, String>();
+        env.put("java.naming.factory.initial", "org.jnp.interfaces.NamingContextFactory");
+        env.put("java.naming.provider.url", "jnp://localhost:1099");
+        env.put("java.naming.factory.url.pkgs", "org.jboss.naming:org.jnp.interfaces");
+        InitialContext context = new InitialContext(env);
+        HornetQJMSConnectionFactory cf = (HornetQJMSConnectionFactory) context.lookup("/ConnectionFactory");
+        c = cf.createConnection();
+        Session session  = c.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        HornetQQueue q = (HornetQQueue) context.lookup("/queue/testQueue");
+        MessageProducer producer = session.createProducer(q);
+        kill("liveA");
+        Thread.sleep(5000);
+        producer.send(session.createTextMessage("got there!"));
+        c.close();
+    }
+
+    private void kill(String nodeName) throws Exception
+    {
+
+         ObjectName on = ObjectName.getInstance("org.hornetq." + nodeName + ":module=Core,type=Server");
+         JMXConnector connector = JMXConnectorFactory.connect(new JMXServiceURL(JMX_URL), new HashMap<String, String>());
+         MBeanServerConnection mbsc = connector.getMBeanServerConnection();
+         HornetQServerControl serverControl = MBeanServerInvocationHandler.newProxyInstance(mbsc,
+                                                                                            on,
+                                                                                            HornetQServerControl.class,
+                                                                                            false);
+        try
+        {
+            serverControl.forceFailover();
+        }
+        catch (Exception e)
+        {
+            //we expect this dont we
+        }
+    }
+
+
+}
